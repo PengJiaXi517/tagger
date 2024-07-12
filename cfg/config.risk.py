487ed22cf625a282374cfec1e5e0d0c725583437
Ness Hu <ness.hu@robosense.cn>
2024-07-12 18:52:03 +0800

    Feat: update risk

--- cfg/config.risk.py
+++ cfg/config.risk.py
@@ -1,10 +1,10 @@
-
 tag_pipelines = {
-    'path_risk': {},
-    'map_risk': {},
-    'condition_risk_check': {},
-    'ego_state_location': {},
-    'ego_state_speed': {},
-    'ego_state_map_environment': {},
-    'ego_state_obs_environment': {}
+    "path_risk": {},
+    "map_risk": {},
+    "condition_risk_check": {},
+    "ego_state_location": {},
+    "ego_state_speed": {},
+    "ego_state_map_environment": {},
+    "ego_state_obs_environment": {},
+    "future_path_tag": {},
 }
