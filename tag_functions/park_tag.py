08abc3b624db47aa2317b09f0378584c66ef0c10
claude.wang <claude.wang@robosense.cn>
2024-10-17 17:55:44 +0800

    Fix: error, ego_s may be None

--- tag_functions/park_tag.py
+++ tag_functions/park_tag.py
@@ -238,7 +238,8 @@ def check_front_car(obstacles) -> Union[int, None]:
             ego_s = obstacles[key]["decision"]["ego_s"]
 
     # relative obs s
-    obs_info["s"] -= ego_s
+    if ego_s is not None:
+        obs_info["s"] -= ego_s
 
     # filter obs
     filtered_obs_info = obs_info[(np.abs(obs_info["l"]) < 1.5) & (obs_info["s"] > 0)]
